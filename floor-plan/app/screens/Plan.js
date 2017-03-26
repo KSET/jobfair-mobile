9466f6191e32a416f6400aee50432ba16d3b4b86
morrigan <belma.dzi@gmail.com>
2017-03-26 18:18:10 +0200

    Fixed map pins in webview

--- floor-plan/app/screens/Plan.js
+++ floor-plan/app/screens/Plan.js
@@ -5,11 +5,39 @@ import React, {
 import { WebView } from 'react-native';
 
 export default class Plan extends Component {
+  onNavigationStateChange(navState) {
+      console.log(navState.title);
+  }
+
   render() {
-    return (
+    const javascript = `
+    function putPin(e) {
+        var offsetY = 48;
+        var offsetX = 24;
+        var coords = e.target.coords.split(',');
+        var x = (Number(coords[0]) + Number(coords[2])) / 2 + 7;
+        var y = (Number(coords[1]) + Number(coords[3])) / 2 + 5;
+        document.getElementById('container').insertAdjacentHTML('beforeend', '<img id="pin" src="./pin.png" style="width:auto;height:auto;position:absolute; left:' + (x - offsetX) + 'px; top:' + (y - offsetY) + 'px;" />');
+    }
+
+    function resolve(e) {
+        if (document.getElementById('pin')) {
+            document.getElementById('pin').remove();
+        }
+        putPin(e);
+    }
+    
+    var areas = document.querySelectorAll('area');
+    for (var i = 0; i < areas.length; i++) {
+        areas[i].addEventListener("click", resolve);
+        areas[i].addEventListener("tap", resolve);
+    }
+    `;
+
+      return (
         <WebView
             source={require('../assets/map.html')}
-            style={{marginTop: 20}}
+            injectedJavaScript={javascript}
         />
     );
   }
